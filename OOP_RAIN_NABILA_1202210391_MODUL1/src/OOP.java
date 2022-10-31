65adab3b4e20e7f344726df23d2ff30a21fe4b54
nabnabilaa <116347520+nabnabilaa@users.noreply.github.com>
2022-10-31 08:44:29 +0700

    Add files via upload

---
+++ OOP_RAIN_NABILA_1202210391_MODUL1/src/OOP.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public interface OOP {
+    public void bakso();
+    public void esteh();
+    public void brownies();
+    
+    
+    
+}
---
+++ OOP_RAIN_NABILA_1202210391_MODUL1/src/oop_rain_nabila_1202210391_modul1/OOP_RAIN_NABILA_1202210391_MODUL1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package oop_rain_nabila_1202210391_modul1;
+
+import java.util.Scanner;
+
+public class OOP_RAIN_NABILA_1202210391_MODUL1 {
+
+    public static void main(String[] args) {
+    }
+        
+       
+     
+       Scanner nm = new Scanner (System.in);
+        System.out.println( "Nama :");
+        string nama = nm.nextstring();
+        
+        Scanner nh = new Scanner (System.in);
+        System.out.println( "No Handphone :");
+        int naha = nh.nextint();
+        
+        Scanner nm = new Scanner (System.in);
+        System.out.println( "Nama :");
+        string nama = nm.nextInt();
+       
+}
